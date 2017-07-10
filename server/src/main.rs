f36f7dd02f2247c1a5254487bc7ecbe165c534c3
Filippo Cucchetto <filippocucchetto@gmail.com>
2017-07-10 23:23:07 +0200

    [Server] Code clean up

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,25 @@
 extern crate protobuf;
 mod protos;
 
+use std::result::Result;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use protos::message::{Message, Message_Type, GetEventRequest, GetEventReply};
-
-type Result = std::result::Result<(), Error>;
 
 #[derive(Debug)]
 enum Error {
     IoError(std::io::Error),
     DecodeError,
-    MagicNumberMismatch
+    MagicNumberMismatch,
+    ProtobufError(protobuf::ProtobufError),
 }
 
 impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Error {
-        Error::IoError(err)
-    }
+    fn from(err: std::io::Error) -> Error { Error::IoError(err) }
+}
+
+impl From<protobuf::ProtobufError> for Error {
+    fn from(err: protobuf::ProtobufError) -> Error { Error::ProtobufError(err) }
 }
 
 fn encode_u32(value: u32, buffer: &mut[u8]) {
@@ -27,7 +29,7 @@ fn encode_u32(value: u32, buffer: &mut[u8]) {
     buffer[3] = (value & 0x000000FF) as u8;
 }
 
-fn decode_u32(buffer: &[u8]) -> std::result::Result<u32, Error> {
+fn decode_u32(buffer: &[u8]) -> Result<u32, Error> {
     if buffer.len() < 4 {
         return Err(Error::DecodeError);
     }
@@ -46,38 +48,42 @@ fn on_get_events_request(stream: &mut TcpStream, request: &GetEventRequest) {
     message.set_getEventReply(GetEventReply::new());
 }
 
-fn on_message_received(stream: &mut TcpStream, message: Vec<u8>) {
-    let data: &[u8] = &message;
-    if let Ok(message) = protobuf::parse_from_bytes::<Message>(data) {
-        match message.get_field_type() {
-            Message_Type::GET_EVENTS_REQUEST => on_get_events_request(stream, message.get_getEventRequest()),
-            _ => ()
-        }
+fn on_message_received(stream: &mut TcpStream, message: Message) {
+    match message.get_field_type() {
+        Message_Type::GET_EVENTS_REQUEST => on_get_events_request(stream, message.get_getEventRequest()),
+        _ => ()
     }
 }
 
-fn handle_client(mut stream: TcpStream) -> Result {
-    const BUFFER_SIZE: usize = 512;
+fn receive_message(buffer: &mut[u8], stream: &mut TcpStream) -> Result<Message, Error> {
     const EXPECTED_MAGIC_NUMBER: u32 = 12345;
+
+    stream.read_exact(&mut buffer[0..4])?;
+    let magic_number = decode_u32(&buffer[0..4])?;
+    if magic_number != EXPECTED_MAGIC_NUMBER {
+        return Err(Error::MagicNumberMismatch);
+    };
+
+    stream.read_exact(&mut buffer[0..4])?;
+    let size = decode_u32(&buffer[0..4])? as usize;
+
+    let mut message: Vec<u8> = Vec::new();
+    message.reserve(size);
+    while message.len() < size {
+        let num_to_read = std::cmp::min(buffer.len(), size - message.len());
+        let (slice, _) = buffer.split_at_mut(num_to_read);
+        stream.read_exact(slice)?;
+        message.extend_from_slice(slice);
+    }
+    let message = protobuf::parse_from_bytes::<Message>(&message)?;
+    Ok(message)
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    const BUFFER_SIZE: usize = 512;
     let mut buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
     loop {
-        stream.read_exact(&mut buffer[0..4])?;
-        let magic_number = decode_u32(&buffer[0..4])?;
-        if magic_number != EXPECTED_MAGIC_NUMBER {
-            return Err(Error::MagicNumberMismatch);
-        };
-
-        stream.read_exact(&mut buffer[0..4])?;
-        let size = decode_u32(&buffer[0..4])? as usize;
-
-        let mut message: Vec<u8> = Vec::new();
-        message.reserve(size);
-        while message.len() < size {
-            let num_to_read = std::cmp::min(BUFFER_SIZE, size - message.len());
-            let (slice, _) = buffer.split_at_mut(num_to_read);
-            stream.read_exact(slice)?;
-            message.extend_from_slice(slice);
-        }
+        let message = receive_message(&mut buffer, &mut stream)?;
         on_message_received(&mut stream, message);
     }
 }
