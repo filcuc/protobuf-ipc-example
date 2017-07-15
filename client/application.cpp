a0b771e5e3bf89541264b8a17ad2c4656a629921
Filippo Cucchetto <filippocucchetto@gmail.com>
2017-07-15 11:12:27 +0200

    [Client] Little code cleanup

--- client/application.cpp
+++ client/application.cpp
@@ -9,14 +9,14 @@ namespace protobuf_client_example {
   namespace {
     const uint32_t MAGIC_NUMBER = 12345;
 
-    void encode(char* ptr, uint32_t data) {
+    void encodeToBigEndian(char* ptr, uint32_t data) {
       ptr[0] = static_cast<char>((data & 0xFF000000) >> 24); 
       ptr[1] = static_cast<char>((data & 0x00FF0000) >> 16); 
       ptr[2] = static_cast<char>((data & 0x0000FF00) >> 8); 
       ptr[3] = static_cast<char>((data & 0x000000FF));
     }
 
-    uint32_t decode(char* ptr) {
+    uint32_t decodeFromBigEndian(char* ptr) {
         uint32_t data = static_cast<uint32_t>(ptr[0]);
         data = data << 8;
         data += static_cast<uint32_t>(ptr[1]);
@@ -58,10 +58,10 @@ namespace protobuf_client_example {
       QByteArray data = socket.readAll();
       if (data.size() < 4)
           return;
-      uint32_t magic_number = decode(data.data());
+      uint32_t magic_number = decodeFromBigEndian(data.data());
       if (data.size() < 8)
           return;
-      uint32_t message_size = decode(data.data() + 4);
+      uint32_t message_size = decodeFromBigEndian(data.data() + 4);
       if (data.size() < (8 + message_size))
           return;
       protocol::Message message;
@@ -72,8 +72,8 @@ namespace protobuf_client_example {
   void Application::send(const protocol::Message &message) {
     const int message_size = 2 * sizeof(uint32_t) + message.ByteSize();
     sendBuffer.resize(message_size);
-    encode(&sendBuffer[0], MAGIC_NUMBER);
-    encode(&sendBuffer[4], message.ByteSize());
+    encodeToBigEndian(&sendBuffer[0], MAGIC_NUMBER);
+    encodeToBigEndian(&sendBuffer[4], message.ByteSize());
     const bool ok = message.SerializeToArray(&sendBuffer[8], sendBuffer.size() - 8);
     if (ok) {
       qDebug() << "Sending a message through the socket";
@@ -85,7 +85,7 @@ namespace protobuf_client_example {
   {
       switch (message.type())
       {
-      case protocol::Message_Type_SET_EVENTS_REPLY:
+      case protocol::Message_Type_GET_EVENTS_REPLY:
           qDebug() << "GetEventsReply received";
           break;
       default:
