89b2acba57c2571b9686ca83b96a541f5db91df6
Filippo Cucchetto <filippocucchetto@gmail.com>
2017-07-15 11:11:56 +0200

    [Server] Code cleanup and use of CodeOutputStream

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,6 +1,6 @@
 use protos::message::{Message, Message_Type, GetEventRequest, GetEventReply};
 use std::{io, cmp, net};
-use std::io::{Read, Write};
+use std::io::Read;
 use protobuf;
 use protobuf::Message as ProtoBufMessage;
 
@@ -9,6 +9,7 @@ const EXPECTED_MAGIC_NUMBER: u32 = 12345;
 #[derive(Debug)]
 enum Error {
     IoError(io::Error),
+    EncodeError,
     DecodeError,
     MagicNumberMismatch,
     ProtobufError(protobuf::ProtobufError),
@@ -22,14 +23,19 @@ impl From<protobuf::ProtobufError> for Error {
     fn from(err: protobuf::ProtobufError) -> Error { Error::ProtobufError(err) }
 }
 
-fn encode_u32(value: u32, buffer: &mut[u8]) {
+fn encode_u32_to_be(value: u32, buffer: &mut[u8]) -> Result<(), Error> {
+    if buffer.len() < 4 {
+        return Err(Error::EncodeError);
+    }
+    let value = value.to_le();
     buffer[0] = ((value & 0xFF000000) >> 24) as u8;
     buffer[1] = ((value & 0x00FF0000) >> 16) as u8;
     buffer[2] = ((value & 0x0000FF00) >> 8) as u8;
     buffer[3] = (value & 0x000000FF) as u8;
+    Ok(())
 }
 
-fn decode_u32(buffer: &[u8]) -> Result<u32, Error> {
+fn decode_u32_from_be(buffer: &[u8]) -> Result<u32, Error> {
     if buffer.len() < 4 {
         return Err(Error::DecodeError);
     }
@@ -68,13 +74,13 @@ impl Client {
 
     fn receive_message(&mut self) -> Result<Message, Error> {
         self.stream.read_exact(&mut self.buffer[0..4])?;
-        let magic_number = decode_u32(&self.buffer[0..4])?;
+        let magic_number = decode_u32_from_be(&self.buffer[0..4])?;
         if magic_number != EXPECTED_MAGIC_NUMBER {
             return Err(Error::MagicNumberMismatch);
         };
 
         self.stream.read_exact(&mut self.buffer[0..4])?;
-        let size = decode_u32(&self.buffer[0..4])? as usize;
+        let size = decode_u32_from_be(&self.buffer[0..4])? as usize;
 
         let mut message: Vec<u8> = Vec::new();
         message.reserve(size);
@@ -90,31 +96,33 @@ impl Client {
 
     fn on_message_received(&mut self, message: Message) {
         match message.get_field_type() {
-            Message_Type::GET_EVENTS_REQUEST => self.on_get_events_request(message.get_getEventRequest()),
+            Message_Type::GET_EVENTS_REQUEST => self.on_get_events_request(message.get_getEventRequest()).unwrap(),
             _ => ()
         }
     }
 
-    fn on_get_events_request(&mut self, request: &GetEventRequest) {
-        println!("GetEventsRequest {:?}", request);
+    fn on_get_events_request(&mut self, request: &GetEventRequest) -> Result<(), Error> {
         let mut reply = Message::new();
-        reply.set_field_type(Message_Type::SET_EVENTS_REPLY);
+        reply.set_field_type(Message_Type::GET_EVENTS_REPLY);
         reply.set_getEventReply(GetEventReply::new());
-        self.send_message(&reply);
+        self.send_message(&reply)?;
+        Ok(())
     }
 
-    fn send_message(&mut self, message: &Message) {
-        let size: u32 = message.compute_size();
-        let mut buffer: Vec<u8> = Vec::new();
-        buffer.resize(8, 0);
-        // Encode magic number
-        encode_u32(EXPECTED_MAGIC_NUMBER, &mut buffer[0..4]);
-        // Encode message size
-        encode_u32(size, &mut buffer[4..8]);
-        // Encode message
-        let mut bytes = message.write_to_bytes().unwrap();
-        buffer.append(&mut bytes);
-        // Write message
-        self.stream.write_all(&mut buffer).unwrap();
+    fn send_message(&mut self, message: &Message) -> Result<(), Error> {
+        let mut buffer: [u8; 4] = [0; 4];
+        // Wrap stream
+        let mut stream = protobuf::CodedOutputStream::new(&mut self.stream);
+        // Write magic number
+        encode_u32_to_be(EXPECTED_MAGIC_NUMBER, &mut buffer[0..4])?;
+        stream.write_raw_bytes(&buffer)?;
+        // Write message size
+        encode_u32_to_be(message.compute_size(), &mut buffer[0..4])?;
+        stream.write_raw_bytes(&buffer)?;
+        // Write message 
+        message.write_to(&mut stream)?;
+        // Flush
+        stream.flush()?;
+        Ok(())
     }
 }
