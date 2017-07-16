baba622635357156c1e564077b79b2bf4b844d5c
Filippo Cucchetto <filippocucchetto@gmail.com>
2017-07-16 15:40:56 +0200

    [Client] Added command line options

--- client/main.cpp
+++ client/main.cpp
@@ -1,9 +1,30 @@
-#include <QtCore>
+#include <QCoreApplication>
+#include <QCommandLineParser>
 #include "application.h"
 
 int main(int argc, char *argv[]) {
   QCoreApplication app(argc, argv);
+  app.setApplicationName("Client");
+  app.setApplicationVersion("0.0.1");
+
+  QCommandLineOption portOption {"port", "Connect port", "number", "56323"};
+  QCommandLineOption hostnameOption {"hostname", "Connect hostname", "hostname", "localhost"};
+
+  QCommandLineParser parser;
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addOption(portOption);
+  parser.addOption(hostnameOption);
+
+  parser.process(app);
+
+  const QString hostname = parser.value(hostnameOption);
+  bool ok = false;
+  const int port = parser.value(portOption).toInt(&ok);
+  if (!ok)
+      return -1;
+
   protobuf_client_example::Application a;
-  a.start("localhost", 56323);
+  a.start(hostname, port);
   return app.exec();
 }
